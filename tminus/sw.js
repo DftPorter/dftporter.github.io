3ba035f4108affcfab9fa6bd980706b64ccb8a6f
DftPorter <porter.allen@gmail.com>
2026-04-23 07:12:32 -0400

    Add files via upload

---
+++ tminus/sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'countdowns-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://fonts.googleapis.com/css2?family=Outfit:wght@200;300;400;500;600&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      // Return cached version, but also refresh in background
+      const network = fetch(e.request).then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => {});
+      return cached || network;
+    })
+  );
+});
