c154863295e565a62ac4cf764b7c924ffa5de631
DftPorter <porter.allen@gmail.com>
2026-06-25 12:35:22 -0400

    Add files via upload

--- tides/sw.js
+++ tides/sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'tides-v1';
-const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './waves.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
